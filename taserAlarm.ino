a29497586a8c46a294bae1e0b7bb343fc0584868
Kelsey Williams <ckelseytheewilliams@gmail.com>
2020-02-19 17:46:21 -0800

    updated code; added EEPROM save feature

--- taserAlarm.ino
+++ taserAlarm.ino
@@ -1,7 +1,3 @@
-//lol.
-//You fucking clicked the link to read my code
-//You fucking nerd.
-
 /* Author: Kelsey Williams
  * Yeah, so, this is an alarm clock that
  * tases you instead of beeping.  I used
@@ -23,10 +19,12 @@
  * button to stop the alarm = 7
  * signal pin for relay = 6
  * pin for "armed" led = 5
+ * Button to write clock and alarm to EEPROM = 4
  */
  /*Also. Fucking remember that the documentation for Button.h 
   * on arduino playground is utter horsehsit.  
   Don't even bother reading it.*/
+#include <EEPROM.h>
 #include <Button.h>
 #include <TM1637Display.h>
 
@@ -44,7 +42,7 @@ uint16_t RELAY = 6;
 bool swtkey = true;
 
 // The last pressed button
-//P.S. -1 equates to null in this case as 0 is a valid character around these parts
+//P.S. -1 equates to null in this case as 0 is a valid integer around these parts
 int16_t key = -1;
 
 Button swt = Button(11);
@@ -52,14 +50,24 @@ Button inch = Button(10);
 Button incm = Button(9);
 Button stt = Button(8);
 Button stp = Button(7);
+Button wrt = Button(4);
 
+// Get saved Time from EEPROM and reconstruct from 8bit int to 16 bit
+uint16_t loadT = ((uint16_t)EEPROM.read(0) << 8) | EEPROM.read(1);
+uint16_t loadA = ((uint16_t)EEPROM.read(2) << 8) | EEPROM.read(3);
+
+// Unformat time saved to EEPROM back to minutes and hours
 // Clock time
-uint16_t minutes = 0;
-uint16_t hours =  12;
+uint16_t hours;
+uint16_t minutes;
 
 // Alarm time
-uint16_t aminutes = 0;
-uint16_t ahours = 12;
+uint16_t ahours;
+uint16_t aminutes;
+
+// Initialize Time
+uint16_t Time;
+uint16_t aTime;
 
 // Alarm booleans
 bool alarmOn = false;
@@ -67,7 +75,7 @@ bool alarmTripped = false;
 
 // Main alarm clock time
 uint32_t delta = millis();
-uint32_t elapsedMilliseconds = 0;
+uint32_t elapMillis = 0;
 
 // Time to measure pulses between zaps
 uint32_t trippedTime;
@@ -82,70 +90,105 @@ void setup() {
   incm.begin();
   stt.begin();
   stp.begin();
+  wrt.begin();
+
+   /* If the EEPROM has never been written to before, it should
+   *  contain the value 255 in each address.  So when we append
+   *  two bytes we will get ffff hex, which run through our 
+   *  arithmetic will result in a floating point number, upgrading
+   *  our int to a float as C++ does; something I really  don't want.  
+   *  This if statement is an attempt to protect the program from this
+   *  on a first time EEPROM read.
+   */
+  if(loadT != 0xffff){
+    hours =  loadT / 100;
+    minutes = loadT - (hours * 100);
+    
+    // Alarm time
+    ahours = loadA / 100;
+    aminutes = loadA - (loadA * 100);
+  }
+  else{
+    hours = 12;
+    minutes = 0;
+    ahours = 12;
+    aminutes = 0;
+  }
   
   pinMode(ALED, OUTPUT);
   pinMode(RELAY, OUTPUT);
 
   // Intialize the display
   display.clear();
-  delay(1000);
-  display.setBrightness(2);
+  display.setBrightness(0);
 }
 void loop() {
   keyListener();
   // Count the time in milliseconds
-  elapsedMilliseconds = millis() - delta;
+  elapMillis = millis() - delta;
   countTime();
   // Display the time
   if(swtkey){
-    DisplayTime(hours, minutes);
-    if(key != -1){
-      switch(key){
-      case 1:
-        hours++;
-        key = -1;
-        break;
-      case 2:
-        minutes++;
-        key = -1;
-        break;
-      default:
-        break;
-      }
+    DisplayTime();
+    switch(key){
+    case 1:
+      hours++;
+      // Display time on a millis timer.  Delay messes it up
+      // DisplayTime() therefore doesn't work and we just got
+      // to go to the source. 
+      display.showNumberDec(Time, true, 4, 0);
+      delay(250);
+      break;
+    case 2:
+      minutes++;
+      display.showNumberDec(Time, true, 4, 0);
+      delay(250);
+      break;
+    default:
+      break;
     }
   }
   if(!swtkey){
-    DisplayAlarm(ahours, aminutes);
-    if(key != -1){
-      switch(key){
-      case 1:
-        ahours++;
-        key = -1;
-        alarmTripped = false;
-        break;
-      case 2:
-        aminutes++;
-        key = -1;
-        alarmTripped = false;
-        break;
-      default:
-         break;
-      }
+    DisplayAlarm();
+    switch(key){
+    case 1:
+      ahours++;
+      DisplayAlarm();
+      delay(250);
+      break;
+    case 2:
+      aminutes++;
+      DisplayAlarm();
+      delay(250);
+      break;
+    default:
+       break;
     }
   }
   ALARM();
   if(alarmTripped){
-    tripped();
+    if(elapMillis %3000 == 0){
+      PORTD = PORTD | 0x40;
+    }
+    if(elapMillis %3010 == 0){
+      PORTD = PORTD & 0xbf;
+    }
   }
   if(!alarmTripped){
     //ensure the relay got shut off
-    digitalWrite(RELAY, LOW);
+    /* I use direct port manipulation here in order
+     *  to cut down on time spent making sure the 
+     *  relay is truly off.  I use a hexidecimal mask
+     *  in order to make sure only pin 6 is switched
+     *  low.
+     */
+    PORTD = PORTD & 0xbf; //0xbf = 1011 1111
   }
 }
 
 void countTime(){
   // One minute passes
-  if(elapsedMilliseconds > 59000){
+  if(elapMillis > 59000){
     minutes++;
     delta = millis();
   }
@@ -205,23 +248,35 @@ void keyListener(){
     // Stop alarm
     key = 4;
   }
+  if(inch.released() || incm.released()){
+    key = -1;
+  }
+  if(wrt.pressed()){
+    // Break 16bit times into 8bit for EEPROM storage
+    uint8_t TimeB = (uint8_t)Time;
+    uint8_t TimeA = (uint8_t)(Time >> 8);
+    uint8_t aTimeB = (uint8_t)aTime;
+    uint8_t aTimeA = (uint8_t)(aTime >> 8);
+    uint32_t vals[] = {TimeA, TimeB, aTimeA, aTimeB};
+    for(uint16_t i = 0; i < 4; i++){
+      EEPROM.write(i, vals[i]);
+    }
+  }
 }
 
-void DisplayTime(uint16_t h, uint16_t m){
-  // Format hours and seconds for clock display
-  uint16_t Time = ((h*100)+m);
-  if(elapsedMilliseconds %500 == 0){
+void DisplayTime(){
+  Time = ((hours*100)+minutes);
+  if(elapMillis %500 == 0){
     display.showNumberDecEx(Time, 0b11100000, true, 4, 0);
   }
-  if(elapsedMilliseconds %1000 == 0){
+  if(elapMillis %1000 == 0){
     display.showNumberDec(Time, true, 4, 0);
   }
 }
 
-void DisplayAlarm(uint16_t h, uint16_t m){
-    // Format hours and seconds for clock display
-    uint16_t Time = ((h*100)+m);
-    display.showNumberDecEx(Time, 0b11100000, true, 4, 0);
+void DisplayAlarm(){
+  aTime = ((ahours*100)+aminutes);
+  display.showNumberDecEx(aTime, 0b11100000, true, 4, 0);
 }
 void ALARM(){
   if(alarmOn){
@@ -244,39 +299,6 @@ void ALARM(){
       alarmTripped = false;
       key = -1;
       break;
-    default:
-      break;
-  }
-}
-void tripped(){
-  /*This bit of code switches the tazer on after about 250 ms
-   * and then switches it off for another 250 and then repeats
-   * cycle.
-   */
-  switch(count){
-    case 0:
-      digitalWrite(RELAY, LOW);
-      trippedTime = millis();
-      count++;
-      break;
-    case 1:
-      if(trippedTime - millis() > 250){
-        digitalWrite(RELAY, HIGH);
-        count = 0;
-        break;
-      }
-      else{
-        break;
-      }
-    case 2:
-      if(trippedTime - millis() > 500){
-         count = 0;
-         trippedTime = millis();
-         break;
-      }
-      else{
-        break;
-      }
     default:
       break;
   }
